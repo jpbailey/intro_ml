ab61e902bce3eb5e2c446fef45444e25891c27ab
Joseph Bailey <joepbailey@gmail.com>
2015-11-15 10:40:40 -0500

    t

---
+++ intro_ml/spam_tag.py
@@ -0,0 +1,14 @@
+import os
+import nltk
+from nltk.corpus import stopwords
+import random
+import shutil
+
+STOPS = stopwords.words('english')
+
+
+# find the top 100 words in the spam and ham train directories
+
+# remove the intersection set
+
+# print out a dictionary where spam entry gets a +1 and each ham entry gets a -1
---
+++ intro_ml/spam_test.py
@@ -0,0 +1,34 @@
+import os
+import nltk
+from nltk.corpus import stopwords
+import random
+import shutil
+
+STOPS = stopwords.words('english')
+
+
+# now see how well we do applying the model to the test set
+
+
+# list_of_files = {}
+# for (dirpath, dirnames, filenames) in os.walk(HAMPATH):
+# 	for filename in filenames:
+# 		list_of_files[filename] = os.sep.join([dirpath, filename])
+# print(len(list_of_files))
+
+# list_of_files = {}
+# for (dirpath, dirnames, filenames) in os.walk(SPAMPATH):
+# 	for filename in filenames: 
+# 		list_of_files[filename] = os.sep.join([dirpath, filename])
+# print(len(list_of_files))
+# print(list_of_files)
+
+
+# for each in list_of_files:
+# 	infile=SPAMPATH+each
+# 	with open (infile, "r") as myfile:
+# 		string=myfile.read().replace('\n', '')
+# 		#print type(string)
+# 		wordlist=nltk.word_tokenize(string)
+# 		filtered_words = [i for i in wordlist if i not in STOPS]
+# 		print(filtered_words)
